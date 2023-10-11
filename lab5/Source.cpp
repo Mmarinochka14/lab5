4245d4aa4b81350ee0770a22d97ec3438ae5df8f
Mmarinochka14 <marinkilll14@gmail.com>
2023-10-11 10:28:26 +0300

    Плохой коммит

---
+++ lab5/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    std::cout << "Введите строку: ";
+    std::string input;
+    std::getline(std::cin, input);
+
+    std::cout << "Строка наоборот: ";
+    for (std::string::reverse_iterator it = input.rbegin(); it != input.rend(); ++it) {
+        std::cout << *it;
+    }
+
+    std::cout << std::endl;
+    return 0;
+}
